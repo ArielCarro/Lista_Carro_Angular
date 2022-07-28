884028a84b8d229a06639cc4eddd4daf3223815e
Ariel Carro <carroariel@gmail.com>
2022-07-28 18:51:05 -0300

    commit del desafio de lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Desafio_op_1';
+  nombre = 'Ariel Carro'
+  alumnos = [
+    {
+      nombres: 'Ariel',
+      apellido: 'Carro',
+      notaFinal:6,
+    },
+    {
+      nombres: 'Juan Gabriel',
+      apellido: 'Perez',
+      notaFinal:8,
+    },
+      {
+        nombres: 'Lisandro Tomas',
+        apellido: 'Gonzales',
+        notaFinal:5,
+      },
+      {
+        nombres: 'Franco',
+        apellido: 'Firpo',
+        notaFinal:3,
+      },
+      {
+        nombres: 'Mariana',
+        apellido: 'Casas',
+        notaFinal:7,
+      },
+  ]
 }
